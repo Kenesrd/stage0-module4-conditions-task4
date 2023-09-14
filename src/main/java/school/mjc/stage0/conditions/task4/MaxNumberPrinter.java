0399af72ad2a232bad5564cf57f64e307de57932
kenes <kenesvip@gmail.com>
2023-09-14 11:56:32 +0500

    Module #4.1 Task 4 Done

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int maxNum = 0;
+        if (first >= second && first >= third){
+            maxNum = first;
+            System.out.println(maxNum);
+        } else if (second >= first && second >= third){
+            maxNum = second;
+            System.out.println(maxNum);
+        } else {
+            maxNum = third;
+            System.out.println(maxNum);
+        }
     }
 }
